a9b14f754c882b79b64e4e093d74dd8a8597fa87
Justin Moon <mail@justinmoon.com>
2025-09-13 12:26:55 +0100

    chore: remove cshim; tests pass with unsigned builder finalize

--- cshim/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CCAN_CONFIG_H
-#define CCAN_CONFIG_H
-// Force conservative path: CCAN sha256 copies input into aligned buffer.
-#define HAVE_UNALIGNED_ACCESS 0
-#endif
-
--- cshim/force_config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "config.h"
-#undef HAVE_UNALIGNED_ACCESS
-#define HAVE_UNALIGNED_ACCESS 0
